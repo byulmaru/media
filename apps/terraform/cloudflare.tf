07b87d2b636cac50597b214461cc62ae6f462607
Jiyu Park <jiyu@byulmaru.co>
2026-02-24 18:21:16 +0900

    R2 관련 환경변수 Terraform에서 자동 생성

--- apps/terraform/cloudflare.tf
+++ apps/terraform/cloudflare.tf
@@ -22,9 +22,25 @@ resource "infisical_project" "media" {
   slug = "media"
 }
 
+resource "infisical_secret" "cdn_domain" {
+  name         = "CDN_DOMAIN"
+  value        = "https://${cloudflare_r2_custom_domain.media.domain}"
+  env_slug     = "prod"
+  workspace_id = infisical_project.media.id
+  folder_path  = "/"
+}
+
 resource "infisical_secret" "r2_endpoint" {
   name         = "R2_ENDPOINT"
-  value        = "https://${cloudflare_r2_custom_domain.media.domain}"
+  value        = "https://${var.cloudflare_account_id}.r2.cloudflarestorage.com"
+  env_slug     = "prod"
+  workspace_id = infisical_project.media.id
+  folder_path  = "/"
+}
+
+resource "infisical_secret" "r2_bucket" {
+  name         = "R2_BUCKET"
+  value        = cloudflare_r2_bucket.media.name
   env_slug     = "prod"
   workspace_id = infisical_project.media.id
   folder_path  = "/"
